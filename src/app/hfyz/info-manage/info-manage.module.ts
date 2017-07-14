2a1285891390c36093e145d944757ce9500d9b85
zxd <2302538216@qq.com>
2017-07-14 11:26:27 +0800

    新增 信息发布的列表查询页面 信息编辑详情页面 #426 信息发布的新增功能 #427 信息发布的编辑功能 #428 信息发布的列表功能 #429 信息发布的删除功能 #455 信息发布的搜索功能

--- src/app/hfyz/info-manage/info-manage.module.ts
+++ src/app/hfyz/info-manage/info-manage.module.ts
@@ -1,23 +1,40 @@
 import {InfoCheckComponent} from './info-check/info-check.component';
 import {InfoListComponent} from './info-list/info-list.component';
-import {InfoPublishComponent} from './info-publish/info-publish.component';
 import {NgModule} from '@angular/core';
-import {DataTableModule, SharedModule, CalendarModule} from 'primeng/primeng';
+import {DataTableModule, SharedModule, CalendarModule, DropdownModule} from 'primeng/primeng';
 import {TreeModule} from 'primeng/primeng';
+import {InfoPublishService} from "./info-publish/info-publish.service";
+import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
+import { CommonModule } from '@angular/common';
+import {InfoPublishModule} from "./info-publish/info-publish.module";
+import { CKEditorModule } from 'ng2-ckeditor';
+import { DatePipe } from '@angular/common';
+
+
 @NgModule({
   declarations: [
-    InfoPublishComponent,
+   // InfoPublishComponent,
     InfoListComponent,
     InfoCheckComponent
   ],
   imports: [
+    InfoPublishModule,
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    DropdownModule,
     DataTableModule
     , SharedModule
     , CalendarModule
     , TreeModule
+    , CKEditorModule
   ],
   exports: [],
-  providers: []
+  providers: [
+    InfoPublishService,
+    DatePipe
+  ]
 })
 export class InfoManageModule {
 }
