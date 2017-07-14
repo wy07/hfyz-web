0a70cd70937a8edd92d4896bd754f441af1b44fe
Wyan_7@163.com <Wyan_7@163.com>
2017-07-14 10:47:57 +0800

    #446  下级企业运营商-搜索功能 #445  下级企业运营商-编辑功能 #444  下级企业运营商-新增功能 #452  下级企业运营商-删除功能

--- src/app/hfyz/platform-manage/platform-manage.component.ts
+++ src/app/hfyz/platform-manage/platform-manage.component.ts
@@ -1,8 +1,7 @@
 import {Component, Injector, OnInit, Renderer} from '@angular/core';
-import {InputTextModule} from 'primeng/primeng';
-import { Router, ActivatedRoute, ResolveData } from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
 import {PlatformManageService} from './shared/platform-manage.service';
-import {LayoutComponent} from '../layout/main-tab/layout.component';
+import {RegularService} from './../common/shared/regular.service';
 import {ToastsManager} from 'ng2-toastr';
 
 @Component({
@@ -14,41 +13,151 @@ export class PlatformManageComponent implements OnInit {
   platformList: any[];
   platform: any;
 
-  platformName: string;
-  platformCode: string;
+  name: string;
+  code: string;
 
   displayDialog: boolean;
-  platformPositions: any[];
   formTitle: string;
   isAdd: boolean;
 
   constructor(private renderer: Renderer
-    , private activatedRoute: ActivatedRoute
     , private toastr: ToastsManager
     , private platformService: PlatformManageService
-    , private inj: Injector) {
+    , private regularService: RegularService) {
     this.displayDialog = false;
-    // this.platformPositions = [];
-    // this.platformPositions.push({ label: 'TOP_BAR', value: 'TOP_BAR' });
-    // this.platformPositions.push({ label: 'SIDE_BAR', value: 'SIDE_BAR' });
+    this.platform = {};
     this.clearForm();
   }
+
   ngOnInit() {
     this.initData();
   }
+
   initData() {
-     this.platformService.list().subscribe(
-     res => {
-     this.platformList = res.platformList;
-     }
-     );
+    this.platformService.list().subscribe(
+      res => {
+        this.platformList = res.platformList;
+      }
+    );
+  }
+
+  // 查询
+  onSearch() {
+    this.platformService.search(this.name, this.code).subscribe(
+      res => {
+        this.platformList = res.platformList;
+      }
+    );
+  }
+
+  // 重置
+  onReset() {
+    this.name = '';
+    this.code = '';
+    this.initData();
+  }
+
+  // 新增
+  onCreat() {
+    this.clearForm();
+    this.formTitle = '平台新增';
+    this.isAdd = true;
+    this.displayDialog = true;
+  }
+
+  onSave() {
+    if (this.validate()) {
+      this.platformService.save(this.platform).subscribe(
+        res => {
+          this.toastr.success('保存成功');
+          this.initData();
+          this.displayDialog = false;
+        }
+      );
+    }
+  }
+
+  // 查看编辑
+  onEdit(platform) {
+    this.clearForm();
+    this.formTitle = `平台编辑`;
+    this.isAdd = false;
+    this.displayDialog = true;
+    this.preEdit(platform.id);
+  }
+
+  preEdit(id) {
+    this.platformService.edit(id).subscribe(
+      res => {
+        if (res.result == 'success') {
+          this.platform = res.platform;
+        } else {
+          this.toastr.error('获取数据失败');
+        }
+      }
+    );
+  }
+
+  update() {
+    if (this.validate()) {
+      this.platformService.update(this.platform.id, this.platform).subscribe(
+        res => {
+          this.toastr.success('保存成功');
+          this.initData();
+          this.displayDialog = false;
+        }
+      );
+    }
+  }
+
+  cancle() {
+    this.displayDialog = false;
+  }
+
+  validate() {
+    if (this.regularService.isBlank(this.platform.ip)) {
+      this.toastr.error('平台IP地址不能为空');
+      return false;
+    }
+    if (this.regularService.isBlank(this.platform.port)) {
+      this.toastr.error('端口号不能为空');
+      return false;
+    }
+    if (this.regularService.isBlank(this.platform.name)) {
+      this.toastr.error('平台名称不能为空');
+      return false;
+    }
+    if (this.regularService.isBlank(this.platform.code)) {
+      this.toastr.error('平台代码不能为空');
+      return false;
+    }
+    if (this.regularService.isBlank(this.platform.contactName)) {
+      this.toastr.error('平台联系人不能为空');
+      return false;
+    }
+    if (this.regularService.isBlank(this.platform.contactPhone)) {
+      this.toastr.error('平台联系电话不能为空');
+      return false;
+    }
+    return true;
+  }
+
+
+  // 删除
+  onDelete(platform) {
+    if (confirm('确认移除"' + platform.name + '"平台？')) {
+      this.platformService.delete(platform.id).subscribe(
+        res => {
+          this.initData();
+          this.toastr.info(`移除数据成功`);
+        }
+      );
+    }
   }
 
 // 清除dialog数据
   clearForm() {
-    // this.filteredParents = [];
-    // this.parent = null;
+    this.platform = {};
   }
-
 
 }
--- src/app/hfyz/platform-manage/shared/platform-manage.service.ts
+++ src/app/hfyz/platform-manage/shared/platform-manage.service.ts
@@ -7,8 +7,30 @@ export class PlatformManageService {
   }
 
   list() {
-    return this.restangular.all('platformManage').customGET('list');
+    return this.restangular.all('platform-manages').customGET('list');
   }
 
+  search(name, code) {
+    // alert(name + code);
+    return this.restangular.all('platform-manages').customGET('search', {name: name, code: code});
+  }
+
+  delete(id) {
+    return this.restangular.one('platform-manages', id).customDELETE('delete', {});
+  }
+
+  edit(id) {
+    return this.restangular.one('platform-manages', id).customGET('edit');
+  }
+
+  save(platform) {
+    return this.restangular.all('platform-manages').customPOST(platform, 'save');
+  }
+
+  update(id, formData) {
+    // console.log(`update:${id}`)
+    // console.log(`formData:${JSON.stringify(formData)}`)
+    return this.restangular.one('platform-manages', id).customPOST(formData, 'update');
+  }
 
 }
