4ec07f804259a8b63e005c3b00cb48630539fd59
jiruiyao <jiruiyao@git.cczet.com>
2017-08-02 18:23:54 +0800

    新增隐患整改

--- src/app/hfyz/hidden-danger/hidden-danger.component.ts
+++ src/app/hfyz/hidden-danger/hidden-danger.component.ts
@@ -4,6 +4,7 @@ import {ToastsManager} from 'ng2-toastr';
 import {HiddenDangerService} from './shared/hidden-danger.service';
 import DateTimeFormat = Intl.DateTimeFormat;
 import { DatePipe } from '@angular/common';
+import {TdLoadingService} from '@covalent/core';
 @Component({
   selector: 'app-hidden-danger',
   templateUrl: './hidden-danger.component.html',
@@ -12,7 +13,7 @@ import { DatePipe } from '@angular/common';
 export class HiddenDangerComponent implements OnInit {
   hiddenDangerList: any;
   hiddenDanger: any;
-  displayDialog: boolean;
+  edit: boolean;
   hiddenDangerTitle: string;
   isAdd: boolean;
   max: number;
@@ -26,13 +27,15 @@ export class HiddenDangerComponent implements OnInit {
   endDate: any;
   maxDate: any;
   isDetails: boolean;
+  displayDialog: boolean;
   constructor(
      private _toastr: ToastsManager
     , private _hiddenDangerService: HiddenDangerService
     , private _regularService: RegularService
     , private _datePipe: DatePipe
+    , private _loadingService: TdLoadingService
     ) {
-    this.displayDialog = false;
+    this.edit = false;
     this.hiddenDanger = {};
     this.clear();
     this.max = 10;
@@ -42,6 +45,7 @@ export class HiddenDangerComponent implements OnInit {
     this.isDetails = false;
     this.maxDate = new Date();
     this.reply = null;
+    this.displayDialog = false;
   }
 
   ngOnInit() {
@@ -49,7 +53,7 @@ export class HiddenDangerComponent implements OnInit {
   }
 
   initData(offset = 0) {
-    if (!this.validation()) {
+    if (!this.validation_search()) {
       return false;
     }
     let sd = '';
@@ -60,8 +64,10 @@ export class HiddenDangerComponent implements OnInit {
     if (!this._regularService.isBlank(this.endDate)) {
       ed = this._datePipe.transform(this.endDate, 'yyyy-MM-dd HH:mm');
     }
+    this._loadingService.register();
     this._hiddenDangerService.list(this.max, offset, this.company, sd, ed).subscribe(
       res => {
+        this._loadingService.resolve();
         this.hiddenDangerList = res.hiddenDangerList;
         this.total = res.total;
       }
@@ -82,44 +88,52 @@ export class HiddenDangerComponent implements OnInit {
     this.clear();
     this.hiddenDangerTitle = '新增隐患整改单';
     this.isAdd = true;
-    this.displayDialog = true;
+    this.edit = true;
   }
 
   onSave() {
     if (this.validation()) {
-      this.hiddenDanger['inspection'] = this._datePipe.transform(this.inspection, 'yyyy-MM-dd HH:mm');
-      this.hiddenDanger['dealine'] = this._datePipe.transform(this.dealine, 'yyyy-MM-dd HH:mm');
+      this.hiddenDanger.inspection = this._datePipe.transform(this.inspection, 'yyyy-MM-dd HH:mm');
+      this.hiddenDanger.dealine = this._datePipe.transform(this.dealine, 'yyyy-MM-dd HH:mm');
+      this._loadingService.register();
       this._hiddenDangerService.save(this.hiddenDanger).subscribe(
         res => {
+          this._loadingService.resolve();
           this._toastr.success('保存成功');
           this.initData();
-          this.displayDialog = false;
+          this.edit = false;
         }
       );
     }
+    this.displayDialog = false;
   }
-
 
   onEdit(hiddenDanger) {
     this.clear();
-    this.hiddenDangerTitle = `编辑隐患整改单`;
+    this.hiddenDangerTitle = '编辑隐患整改单';
     this.isAdd = false;
-    this.displayDialog = true;
+    this.edit = true;
     this.preEdit(hiddenDanger.id);
   }
 
   preEdit(id) {
-    if (this.displayDialog === false) {
+    if (this.edit === false) {
       this.isDetails = true;
+      this.hiddenDangerTitle = '隐患整改单详情';
     }
+    this._loadingService.register();
     this._hiddenDangerService.edit(id).subscribe(
       res => {
+        this._loadingService.resolve();
         if (res.result === 'success') {
           this.hiddenDanger = res.hiddenDanger;
           this.inspection = new Date(this.hiddenDanger.inspectionDate);
           this.dealine = new Date(this.hiddenDanger.dealineDate);
+          delete this.hiddenDanger['inspectionDate'];
+          delete this.hiddenDanger['dealineDate'];
           if (this.hiddenDanger.replyDate !== null) {
             this.reply = new Date(this.hiddenDanger.replyDate)
+            delete this.hiddenDanger['replyDate'];
           }
         } else {
           this._toastr.error('获取数据失败');
@@ -130,24 +144,36 @@ export class HiddenDangerComponent implements OnInit {
 
   update() {
     if (this.validation()) {
-      this.hiddenDanger['inspection'] = this._datePipe.transform(this.inspection, 'yyyy-MM-dd HH:mm');
-      this.hiddenDanger['dealine'] = this._datePipe.transform(this.dealine, 'yyyy-MM-dd HH:mm');
-      this.hiddenDanger['reply'] = this._datePipe.transform(this.reply, 'yyyy-MM-dd HH:mm');
+      this.hiddenDanger.inspection = this._datePipe.transform(this.inspection, 'yyyy-MM-dd HH:mm');
+      this.hiddenDanger.dealine = this._datePipe.transform(this.dealine, 'yyyy-MM-dd HH:mm');
+      this.hiddenDanger.reply = this._datePipe.transform(this.reply, 'yyyy-MM-dd HH:mm');
+      this._loadingService.register();
       this._hiddenDangerService.update(this.hiddenDanger.id, this.hiddenDanger).subscribe(
         res => {
+          this._loadingService.resolve();
           this._toastr.success('保存成功');
           this.initData();
-          this.displayDialog = false;
+          this.edit = false;
+        }
+      );
+    }
+    this.displayDialog = false;
+  }
+
+  onDelete(hiddenDanger) {
+    if (confirm('确认移除编号为："' + hiddenDanger.billNo + '"的隐患？')) {
+      this._loadingService.register();
+      this._hiddenDangerService.delete(hiddenDanger.id).subscribe(
+        res => {
+          this._loadingService.resolve();
+          this.initData();
+          this._toastr.info('移除数据成功');
         }
       );
     }
   }
 
-  cancle() {
-    this.displayDialog = false;
-  }
-
-  validation() {
+  validation_search() {
     if (!this._regularService.isBlank(this.startDate) && !this._regularService.isBlank(this.endDate)) {
       if (this.endDate.getTime() === this.startDate.getTime()) {
         this._toastr.info('选择的日期不能相同！');
@@ -161,17 +187,53 @@ export class HiddenDangerComponent implements OnInit {
     }
     return true;
   }
-
-
-  onDelete(hiddenDanger) {
-    if (confirm('确认移除编号为："' + hiddenDanger.billNo + '"的隐患？')) {
-      this._hiddenDangerService.delete(hiddenDanger.id).subscribe(
-        res => {
-          this.initData();
-          this._toastr.info(`移除数据成功`);
-        }
-      );
+  validation() {
+    if (this._regularService.isBlank(this.hiddenDanger.enterpirse)) {
+      this._toastr.error('业户名称不能为空');
+      return false;
     }
+    if (this._regularService.isBlank(this.hiddenDanger.examiner)) {
+      this._toastr.error('检查人不能为空');
+      return false;
+    }
+    if (this._regularService.isBlank(this.inspection)) {
+      this._toastr.error('检查日期不能为空');
+      return false;
+    }
+    if (this._regularService.isBlank(this.dealine)) {
+      this._toastr.error('整改期限不能为空');
+      return false;
+    }
+    if (this._regularService.isBlank(this.hiddenDanger.insPosition)) {
+      this._toastr.error('检查地点不能为空');
+      return false;
+    }
+    if (this._regularService.isBlank(this.hiddenDanger.insDesc)) {
+      this._toastr.error('检查内容不能为空');
+      return false;
+    }
+    if (this._regularService.isBlank(this.hiddenDanger.insQuestion)) {
+      this._toastr.error('存在问题不能为空');
+      return false;
+    }
+    if (this._regularService.isBlank(this.hiddenDanger.proPosal)) {
+      this._toastr.error('整改意见不能为空');
+      return false;
+    }
+    if (this._regularService.isBlank(this.hiddenDanger.status)) {
+      this._toastr.error('状态不能为空');
+      return false;
+    }
+    if (this.inspection.getTime() === this.dealine.getTime()) {
+      this._toastr.info('选择的日期不能相同！');
+      return false;
+    }
+
+    if (this.dealine < this.inspection) {
+      this._toastr.info('请选择正确的日期！');
+      return false;
+    }
+    return true;
   }
 
   clear() {
@@ -187,5 +249,16 @@ export class HiddenDangerComponent implements OnInit {
 
   return() {
     this.isDetails = false;
+    this.edit = false;
+  }
+
+  onCancel() {
+    this.displayDialog = false;
+  }
+
+  onSaveNew() {
+    if (this.validation()) {
+      this.displayDialog = true;
+    }
   }
 }
