875a930ea8508f31cd44806696d2287ef240b60f
jiruiyao <jiruiyao@git.cczet.com>
2017-08-03 09:26:34 +0800

    修改工单列表

--- src/app/hfyz/work-order/work-order.component.ts
+++ src/app/hfyz/work-order/work-order.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ToastsManager } from 'ng2-toastr';
-import DateTimeFormat = Intl.DateTimeFormat;
 import {WorkOrderService} from './shared/work-order.service';
 
 @Component({
@@ -14,8 +12,7 @@ export class WorkOrderComponent implements OnInit {
   currentPage: number;
   max: any;
   total: any;
-  constructor(private _toastr: ToastsManager
-    , private _workOrderService: WorkOrderService
+  constructor(private _workOrderService: WorkOrderService
   ) {
     this.workOrderList = [];
     this.max = 10;
@@ -28,8 +25,8 @@ export class WorkOrderComponent implements OnInit {
   initData(offset = 0) {
     this._workOrderService.list(this.max, offset).subscribe(
       res => {
-        this.workOrderList = res.resultList.workOrderList;
-        this.total = res.resultList.total;
+        this.workOrderList = res.workOrderList;
+        this.total = res.total;
       }
     );
   }
--- src/app/hfyz/work-order/work-order.module.ts
+++ src/app/hfyz/work-order/work-order.module.ts
@@ -1,14 +1,12 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpModule } from '@angular/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { DataTableModule, SharedModule, DialogModule, ListboxModule, ButtonModule, DataGridModule, TreeModule, PaginatorModule, CalendarModule } from 'primeng/primeng';
+import { DataTableModule, PaginatorModule } from 'primeng/primeng';
 import { DatePipe } from '@angular/common';
-import { DropdownModule } from 'primeng/primeng';
-import {WorkOrderComponent} from './work-order.component';
-import {CommonModule} from '../common/common.module';
-import {WorkOrderService} from './shared/work-order.service';
+import { WorkOrderComponent } from './work-order.component';
+import { CommonModule } from '../common/common.module';
+import { WorkOrderService } from './shared/work-order.service';
 
 @NgModule({
     declarations: [
@@ -16,21 +14,11 @@ import {WorkOrderService} from './shared/work-order.service';
     ],
     imports: [
         BrowserModule,
-        FormsModule,
         HttpModule,
-        ReactiveFormsModule,
         BrowserAnimationsModule,
         CommonModule,
         DataTableModule,
-        SharedModule,
-        DialogModule,
-        ListboxModule,
-        ButtonModule,
-        DataGridModule,
-        TreeModule,
-        PaginatorModule,
-        CalendarModule,
-        DropdownModule
+        PaginatorModule
     ],
     exports: [
     ],
