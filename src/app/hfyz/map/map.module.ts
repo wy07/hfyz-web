3c3c78a5cc275e546b80c96e48ad234f72e50e20
wuchao <chao.wu@shifudao.com>
2017-08-04 18:19:11 +0800

    动态获取实时监控和实时动态数据

--- src/app/hfyz/map/map.module.ts
+++ src/app/hfyz/map/map.module.ts
@@ -1,30 +1,37 @@
+import { DatePipe } from '@angular/common';
+import { MapTabsBarComponent } from './shared/map-tab-bar/map-tabs-bar.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from './../common/common.module';
 import { NgModule } from '@angular/core';
-import { PanelModule, ButtonModule } from 'primeng/primeng';
+import { PanelModule, ButtonModule, DataTableModule } from 'primeng/primeng';
 import { NullMapComponent } from './nullMap/null-map.component';
 import { MapService } from './shared/map.service';
 import { FormsModule } from '@angular/forms';
 import { MapComponent } from './map/map.component';
+import { TabsModule } from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [
     MapComponent,
-    NullMapComponent
+    NullMapComponent,
+    MapTabsBarComponent
   ],
   imports: [
     BrowserModule,
     PanelModule,
     ButtonModule,
     CommonModule,
-    FormsModule
+    FormsModule,
+    DataTableModule,
+    TabsModule.forRoot()
   ],
   exports: [
+    //   MapComponent
   ],
   providers: [
-    MapService
+    MapService,
+    DatePipe
   ]
 })
 export class MapModule {
-}
-;
+};
