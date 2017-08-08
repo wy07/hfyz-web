cf6586b64e2787020c7f582f9688f095b4a7b519
gaojl <jaylen.gao@hotmail.com>
2017-08-08 10:51:42 +0800

    车辆和业户信息中，增加按资格有效期截至时间条件查询

--- src/app/hfyz/owner-identity/owner-identity.component.ts
+++ src/app/hfyz/owner-identity/owner-identity.component.ts
@@ -1,76 +1,105 @@
 import {Component, OnInit, Renderer} from '@angular/core';
 import {ToastsManager} from 'ng2-toastr';
-import {OwnerIdentityService} from './shared/owner-identity.service';
+import {OwnerIdentityService} from './shared/owner-identity.service'
+import {DatePipe} from '@angular/common';
 
 @Component({
-  selector: 'app-owner-identity',
-  templateUrl: './owner-identity.component.html',
-  styleUrls: ['./owner-identity.component.css']
+    selector: 'app-owner-identity',
+    templateUrl: './owner-identity.component.html',
+    styleUrls: ['./owner-identity.component.css']
 })
 export class OwnerIdentityComponent implements OnInit {
-  ownerList: any[];
-  owner: any;
-  ownerName: any
-  companyCode: any
-  displayDialog: boolean;
-  formTitle: string;
-  max: number;
-  total: number;
-  currentPage: number;
+    ownerList: any[];
+    owner: any;
+    ownerName: any
+    companyCode: any
+    displayDialog: boolean;
+    formTitle: string;
+    max: number;
+    total: number;
+    currentPage: number;
+    dateBegin: Date;
+    dateEnd: Date;
 
-  constructor(private renderer: Renderer
-    , private toastr: ToastsManager
-    , private ownerIdentityService: OwnerIdentityService) {
-    this.displayDialog = false;
-    this.owner = {};
-    this.clearForm();
-    this.max = 10;
-  }
-
-
-  ngOnInit() {
-    this.initData();
-  }
-
-  initData(offset = 0) {
-    this.ownerIdentityService.list(this.max, offset, this.ownerName, this.companyCode).subscribe(
-      res => {
-        this.ownerList = res.ownerList.ownerList;
-        this.total = res.ownerList.total;
-      }
-    );
-  }
-
-  onReset() {
-
-  }
-
-  onView(id) {
-    this.displayDialog = true;
-    this.formTitle = `企业详情`;
-    this.ownerIdentityService.view(id).subscribe(
-      res => {
-        if (res.result == 'success') {
-          this.owner = res.owner;
-        } else {
-          this.toastr.error('获取数据失败');
-        }
-      }
-    );
-  }
-
-  paginate(event) {
-    if (this.currentPage !== event.page) {
-      this.currentPage = event.page;
-      this.initData(this.max * event.page);
+    constructor(private renderer: Renderer
+        , private toastr: ToastsManager
+        , private datePipe: DatePipe
+        , private ownerIdentityService: OwnerIdentityService) {
+        this.displayDialog = false;
+        this.owner = {};
+        this.clearForm();
+        this.max = 10;
     }
-  }
 
-  cancle() {
-    this.displayDialog = false;
-  }
 
-  clearForm() {
+    ngOnInit() {
+        this.initData();
+    }
 
-  }
+    initData(offset = 0) {
+        if (this.validate()) {
+            const begin = this.dateBegin ? this.datePipe.transform(this.dateBegin, 'yyyy-MM-dd HH:mm:ss') : ''
+            const end = this.dateEnd ? this.datePipe.transform(this.dateEnd, 'yyyy-MM-dd HH:mm:ss') : ''
+            this.ownerIdentityService.list(begin, end, this.max, offset, this.ownerName, this.companyCode).subscribe(
+                res => {
+                    this.ownerList = res.ownerList.ownerList;
+                    this.total = res.ownerList.total;
+                }
+            );
+        }
+    }
+
+    onReset() {
+        this.dateBegin = null;
+        this.dateEnd = null;
+        this.ownerName = '';
+        this.companyCode = '';
+    }
+
+    onView(id) {
+        this.displayDialog = true;
+        this.formTitle = `企业详情`;
+        this.ownerIdentityService.view(id).subscribe(
+            res => {
+                if (res.result === 'success') {
+                    this.owner = res.owner;
+                } else {
+                    this.toastr.error('获取数据失败');
+                }
+            }
+        );
+    }
+
+    paginate(event) {
+        if (this.currentPage !== event.page) {
+            this.currentPage = event.page;
+            this.initData(this.max * event.page);
+        }
+    }
+
+    cancle() {
+        this.displayDialog = false;
+    }
+
+    clearForm() {
+
+    }
+
+    /**
+     * 搜索参数验证
+     */
+    validate() {
+        let flag = true
+        if (this.dateBegin && this.dateEnd) {
+            if (this.dateBegin > this.dateEnd) {
+                flag = false;
+                this.toastr.error('开始时间不能大于结束时间！');
+            }
+        }
+        if ((this.dateBegin || this.dateEnd) && !(this.dateBegin && this.dateEnd)) {
+            flag = false
+            this.toastr.error('起止时间必须全部填写！')
+        }
+        return flag
+    }
 }
