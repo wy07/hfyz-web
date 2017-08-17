86c6279bf4ceede58cf0d3d4f9c2bc48c73a65fe
zhangyang <zhangyang@feiyang.co>
2017-08-17 09:27:46 +0800

    修改正式环境变量

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // configFile: '../hfyz/config/config.prod.json',
-  eventBusUrl: 'http://127.0.0.1:8001/eventbus',
+  eventBusUrl: 'http://220.178.80.119:8001/eventbus',
   gatewayServer: 'http://220.178.80.119:7001',
   appbrand: '合肥市道路运输动态监管与服务系统平台'
 };
