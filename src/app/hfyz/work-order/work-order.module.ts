083fbe285e49f18caa53656566d7bb35d9b4bfbe
zhangyang <zhangyang@feiyang.co>
2017-08-24 09:25:10 +0800

    ticket630、631工作流相关

--- src/app/hfyz/work-order/work-order.module.ts
+++ src/app/hfyz/work-order/work-order.module.ts
@@ -1,22 +1,25 @@
+import { WorkOrderFlowService } from './shared/work-order-flow.service';
+import { WorkOrderFlowComponent } from './flow/work-order-flow.component';
 import { CustomDirectiveModule } from './../common/custom-directive.module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {HttpModule} from '@angular/http';
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {DataTableModule, PaginatorModule} from 'primeng/primeng';
-import {DatePipe} from '@angular/common';
-import {WorkOrderComponent} from './list/work-order.component';
-import {WorkOrderService} from './shared/work-order.service';
-import {PendingWorkOrderComponent} from './pending/pending-work-order.component';
-import {FormsModule} from '@angular/forms';
-import {FeedbackWorkOrderComponent} from './feedback/feedback-work-order.component';
-import {TooltipModule} from 'ngx-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { DataTableModule, PaginatorModule } from 'primeng/primeng';
+import { DatePipe } from '@angular/common';
+import { WorkOrderComponent } from './list/work-order.component';
+import { WorkOrderService } from './shared/work-order.service';
+import { PendingWorkOrderComponent } from './pending/pending-work-order.component';
+import { FormsModule } from '@angular/forms';
+import { FeedbackWorkOrderComponent } from './feedback/feedback-work-order.component';
+import { TooltipModule } from 'ngx-bootstrap';
 
 @NgModule({
     declarations: [
         WorkOrderComponent,
         PendingWorkOrderComponent,
-        FeedbackWorkOrderComponent
+        FeedbackWorkOrderComponent,
+        WorkOrderFlowComponent
     ],
     imports: [
         BrowserModule,
@@ -31,7 +34,8 @@ import {TooltipModule} from 'ngx-bootstrap';
     exports: [],
     providers: [
         DatePipe,
-        WorkOrderService
+        WorkOrderService,
+        WorkOrderFlowService
     ]
 })
 export class WorkOrderModule {
