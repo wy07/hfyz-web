4bc31b42aadaa3e27987e1d765fe7faa9804a049
jiruiyao <jiruiyao@git.cczet.com>
2017-09-18 22:08:23 +0800

    删除无用构造器

--- src/app/hfyz/export-and-download/export-and-download.service.ts
+++ src/app/hfyz/export-and-download/export-and-download.service.ts
@@ -7,8 +7,6 @@ const EXCEL_EXTENSION = '.xlsx';
 
 @Injectable()
 export class ExportAndDownloadService {
-    constructor() {
-    }
 
     public exportAsExcelFile(json: any[], excelFileName: string): void {
         const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
